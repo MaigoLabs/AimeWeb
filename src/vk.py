6a83fe8fae5ca3419e26dcb9338ac837f365b002
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-03-22 23:49:29 -0400

    [O] Use enter key by default

--- src/vk.py
+++ src/vk.py
@@ -1,17 +1,14 @@
 import ctypes
-from ctypes import wintypes
 
 # Virtual-Key codes
-# RESERVED: 0xEA
-VK = 0x6B
-KEYEVENTF_KEYUP = 0x0002
+VK = 0x0D
 
 
-def press_key(vk_code = VK):
+def press_key(vk_code=VK):
     scan_code = ctypes.windll.user32.MapVirtualKeyA(vk_code, 0)
     ctypes.windll.user32.keybd_event(vk_code, scan_code, 0, 0)
 
 
-def release_key(vk_code = VK):
+def release_key(vk_code=VK):
     scan_code = ctypes.windll.user32.MapVirtualKeyA(vk_code, 0)
-    ctypes.windll.user32.keybd_event(vk_code, scan_code, KEYEVENTF_KEYUP, 0)
+    ctypes.windll.user32.keybd_event(vk_code, scan_code, 0x0002, 0)
