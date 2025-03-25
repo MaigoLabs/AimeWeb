f27955238f9d42b4c91bb37a67142cc6033f28ce
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-03-25 13:38:56 -0400

    [+] HTTPS

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 import { viteSingleFile } from "vite-plugin-singlefile"
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { https: true },
   plugins: [
     svelte(),
     tailwindcss(),
-    viteSingleFile()
+    viteSingleFile(),
+    mkcert()
   ],
 })
